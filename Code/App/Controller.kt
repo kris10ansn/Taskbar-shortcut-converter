b9841bdb567be3edc43b2b1c3e2547968ba0b783
kris10ansn <40371637+kris10ansn@users.noreply.github.com>
2018-10-17 23:14:12 +0200

    Updated code for all changes made in the past

--- Code/App/Controller.kt
+++ Code/App/Controller.kt
@@ -5,10 +5,64 @@ import javafx.stage.FileChooser
 import javafx.stage.FileChooser.ExtensionFilter
 import java.io.File
 
+var shortcutDir = "${System.getProperty("user.home")}\\TaskbarShortcuts"
+var iconPath:String = "icon.png"
 
 // TODO: Change icon when icon is chosen, image file converter
-
 class Controller {
+
+    fun createShortcut(f:File, iconpath:String) {
+        val ftext = f.readText()
+
+        val start = ftext.indexOf("URL=") + 4
+        var end = ftext.indexOf("IconFile=")
+
+        if(end == -1) end = ftext.length-1
+
+        val url = ftext.substring(start until end)
+
+        // Creates directories if they're not already there
+        File(shortcutDir).mkdirs()
+
+        val shortcut = File("$shortcutDir\\${f.nameWithoutExtension}.lnk")
+
+        val createPs = File("$shortcutDir\\create ${f.nameWithoutExtension} shortcut.ps1")
+        createPs.writeText("" +
+                "\$ComObj = New-Object -ComObject WScript.Shell; " +
+                // Shortcut variable                     // Directory for shortcut
+                "\$ShortCut = \$ComObj.CreateShortcut(\"\$Env:${shortcut.absolutePath}\");" +
+                // Opens cmd (when shortcut is run)
+                "\$ShortCut.TargetPath = \"%windir%\\system32\\cmd.exe\"; " +
+                // Description
+                "\$ShortCut.Description = \"${f.nameWithoutExtension}\"; " +
+                // Runs command then closes prompt ("/c")
+                "\$ShortCut.Arguments = \"/c \"\"start $url\"\"\"; " +
+
+                "\$ShortCut.FullName; \$ShortCut.WindowStyle = 7; " +
+                // Icon path
+                "\$ShortCut.IconLocation = \"$iconpath\";" +
+                // Saves shortcut
+                "\$ShortCut.Save();" +
+                // Deletes ps1 file
+                "Remove-Item \"${createPs.absolutePath}\";"
+        )
+
+        Runtime.getRuntime().exec("Powershell.exe -executionpolicy remotesigned -File \"$createPs\"")
+
+        var i = 0
+        // Waits until the file has been created
+        while(!shortcut.exists()) {
+            i++
+            if(i >= 6000000) {
+                // If it takes too long, an error message is printed out, and the script is stopped
+                println("Shortcut might not have been created. Check out $shortcutDir")
+                return
+            }
+        }
+
+        Runtime.getRuntime().exec("explorer.exe /select, ${shortcut.absolutePath}")
+
+    }
     fun iconButtonClicked() {
         val icon = selectFile(ExtensionFilter("Icon files", "*.ico"), "Select icon")
         if(icon != null) {
@@ -35,52 +89,6 @@ class Controller {
 
     fun openShortcuts() {
         Runtime.getRuntime().exec("explorer.exe $shortcutDir")
-    }
-
-    fun shortcutToThis() {
-
-        // Creates directories if they're not already there
-        File(shortcutDir).mkdirs()
-        val shortcut = File("$shortcutDir\\Taskbar shortcut converter.lnk")
-
-        val createPs = File("$shortcutDir\\create ${shortcut.nameWithoutExtension} shortcut.ps1")
-        createPs.writeText("" +
-                "\$ComObj = New-Object -ComObject WScript.Shell; " +
-                // Shortcut variable                     // Directory for shortcut
-                "\$ShortCut = \$ComObj.CreateShortcut(\"\$Env:${shortcut.absolutePath}\");" +
-                // Opens cmd (when shortcut is run)
-                "\$ShortCut.TargetPath = \"%SystemRoot%\\system32\\cmd.exe\"; " +
-                // Description
-                "\$ShortCut.Description = \"Taskbar shortcut converter\"; " +
-                // Runs jar, then closes the cmd window
-                "\$ShortCut.Arguments = \"/c \"\"${getPath()}\"\"\"; " +
-
-                "\$ShortCut.FullName; \$ShortCut.WindowStyle = 7; " +
-                // Icon path
-                "\$ShortCut.IconLocation = \"$iconPath\";" +
-                // Saves shortcut
-                "\$ShortCut.Save();" +
-                // Deletes ps1 file
-                "\$path = \"${createPs.absolutePath}\";" +
-                "Remove-Item \$path;" +
-                // Deletes bat file
-                "Remove-Item \"$shortcutDir\\run ${createPs.nameWithoutExtension}.bat\";"
-        )
-
-        val runPs = File("$shortcutDir\\run ${createPs.nameWithoutExtension}.bat")
-        runPs.writeText("Powershell.exe -executionpolicy remotesigned -File \"$createPs\"")
-
-        Runtime.getRuntime().exec(runPs.absolutePath)
-
-        var i = 0
-        // Waits until the file has been created
-        while(!shortcut.exists()) {
-            i++
-            if(i >= 6000000) return
-        }
-
-        Runtime.getRuntime().exec("explorer.exe /select, ${shortcut.absolutePath}")
-
     }
 
     private fun getPath() : String {
